dddf3b10af80d97b0e3d2cd420b96414c7b43779
Thobias Baumbach <tbaumbach@hotmail.com>
2018-05-10 22:35:51 +0200

    updated to Angular 6 version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { RouterModule, Routes } from '@angular/router'
 
 
 import { AppComponent } from './app.component';
 
+// Ordningen på all route spelar stor roll då matchning sker uppifrån och neråt. Skulle t.ex. '**' ligga först skulle den fånga alla olika adresser.
+/*const appRoutes: Routes = [
+  { path: 'game/:id',      component: GameComponent },
+  { path: '',
+    redirectTo: '/web',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent } //TODO skapa komponenten.
+];*/
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  //  RouterModule.forRoot(
+  //    appRoutes,
+  //    { enableTracing: true } // <-- debugging purposes only
   ],
   providers: [],
   bootstrap: [AppComponent]
